f96eba12fcfb852418f436c48ffbfa90fd2d761f
TheBookwyrms <blackdragon6493@gmail.com>
2024-12-04 12:21:28 -0500

    small generalization to statistics

--- attempt_3/random_statistics_thing.py
+++ attempt_3/random_statistics_thing.py
@@ -3,12 +3,12 @@ import os
 folders = os.listdir(os.getcwd())
 attempt_3 = f'{os.getcwd()}\{folders[1]}'
 files = os.listdir(attempt_3)
+this_file = os.path.realpath(__file__).split('\\')[-1]
 
 new_files = []
 for file in files:
     if file[(len(file)-3):] == '.py':
         new_files.append(f'{attempt_3}\{file}')
-
 
 total_lines = 0
 fors = 0
@@ -21,7 +21,7 @@ for file in new_files:
         file = file.split('\\')[-1]
         for line in text:
             lines += 1
-            if file != "random_statistics_thing.py":
+            if file != this_file:
                 if "for" in line:
                     fors += 1
                 if "while" in line:
